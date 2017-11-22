d0825e45687c7dd31cd1db3a29b2a368b869ae3a
Owen Cowdry <owencowdry@gmail.com>
2017-11-22 00:06:44 +0000

    Switching out ACTION consts to constant file

--- src/scripts/actions/index.js
+++ src/scripts/actions/index.js
@@ -1,10 +1,16 @@
+import { ACTIONS } from '../constants/index';
+
+const { INCREMENT_COUNTER, DECREMENT_COUNTER } = ACTIONS;
+
+console.log(INCREMENT_COUNTER);
+
 export const incrementCounter = () => {
 	return {
-		type: 'INCREMENT_COUNTER'
+		type: INCREMENT_COUNTER
 	}
 };
 export const decrementCounter = () => {
 	return {
-		type: 'DECREMENT_COUNTER'
+		type: DECREMENT_COUNTER
 	}
 };
