3cd55f1f814486d1422e97bcadd339ff0d94c936
Owen Cowdry <owencowdry@gmail.com>
2018-02-10 16:48:51 +0000

    Upgraged to Webpack 3, turned out to be the way I was referencing React in a file. But we may as well go up to webpack 3 seeing how nothing broke

--- src/scripts/reducers/index.js
+++ src/scripts/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 
-import count from './counter';
+import count from './count';
 
 const rootReducer = combineReducers({
 	count
--- src/scripts/store/configureStore.js
+++ src/scripts/store/configureStore.js
@@ -3,7 +3,7 @@ import { createStore } from 'redux';
 // REDUCERS
 import rootReducer from '../reducers';
 
-// Initialze the STORE
+// Initialize the STORE
 const configureStore = () => createStore(rootReducer);
 
 export default configureStore;
