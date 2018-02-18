21f687961b4c36e1ee2c86a87f8059f11cc88368
Owen Cowdry <owencowdry@gmail.com>
2018-02-18 14:53:52 +0000

    Pulling in markdwon branch

--- src/scripts/components/App.js
+++ src/scripts/components/App.js
@@ -6,7 +6,8 @@ import CounterContainer from './counter/CounterContainer';
 
 const App = () => (
 	<div>
-		{/*<h1>React & Redux Counter Example</h1>*/} 
+		{/*<h1>React & Redux Counter Example</h1>*/}
+		<h1>React & Redux Counter Example</h1>
 		<CounterContainer />
 		<Instructions />
 	</div>
