430767c340246ced109dccb97b019a9d25d237ae
Owen Cowdry <owencowdry@gmail.com>
2018-03-04 19:52:57 +0000

    Added in second test to multi counter to check ID is incrementing correctly

--- src/scripts/reducers/multiCounter.test.js
+++ src/scripts/reducers/multiCounter.test.js
@@ -14,5 +14,26 @@ describe('REDUCERS', () => {
 
 			expect(actualResult).toMatchObject(expectedResult);
 		});
+
+
+		it('should create a new counter object with a correctly incremented ID', () => {
+			const stateBefore = [{
+				id: 0,
+				count: 0
+			}];
+
+			const expectedResult = [{
+				id: 0,
+				count: 0
+			},
+			{
+				id: 1,
+				count: 0
+			}];
+
+			const actualResult = createCounter(stateBefore, { type: TYPE.CREATE_COUNTER });
+
+			expect(actualResult).toMatchObject(expectedResult);
+		});
 	});
 });
