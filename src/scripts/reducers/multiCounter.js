fbf27ed0b83358a38d7ba39da0172086c5fcd9d8
Owen Cowdry <owencowdry@gmail.com>
2018-03-17 23:20:41 +0000

    adjusted tests to work with UUID indexed hash table

--- src/scripts/reducers/multiCounter.js
+++ src/scripts/reducers/multiCounter.js
@@ -6,17 +6,7 @@ import { v4 } from 'uuid';
 export const multiCounter = (state = {}, action) => {
 	switch (action.type) {
 		case TYPE.CREATE_COUNTER:
-
-			// TODO - Lift out to helpers
-			function getMaxId(idList) {
-				return idList.reduce((maxId, currentVal) => {
-					return Math.max(parseInt(currentVal, 10), maxId);
-				}, 0) + 1;
-			};
-
-			const counterKeys = Object.keys(state);
-			const newId = !!counterKeys.length ? getMaxId( counterKeys ) : 0;
-
+			const newId = v4();
 			return	{
 				...state,
 				[newId] : {
--- src/scripts/reducers/multiCounter.test.js
+++ src/scripts/reducers/multiCounter.test.js
@@ -1,114 +1,108 @@
 import { multiCounter } from './multiCounter';
 import { TYPE } from '../constants/index';
+// LIBS
+import { v4 } from 'uuid';
 
 describe('REDUCERS', () => {
 
 	describe('Create counter', () => {
-
 		const stateBefore = {};
-
 		const actualResult = multiCounter(stateBefore, { type: TYPE.CREATE_COUNTER });
 
-		it('Should create a new counter object and add it to the state', () => {
+		it('Should create a new counter object and add it to the state.', () => {
 			expect(typeof actualResult).toEqual('object');
 		});
 
-		it('Should contain an object with the keys, "count" and "id"', () => {
+		it('Should contain an object with the keys, "count" and "id".', () => {
 			const expectedResult = ['count', 'id'].sort();
 			const resultId = Object.keys(actualResult)[0];
 
 			expect(Object.keys(actualResult[resultId]).sort()).toEqual(expectedResult);
 		});
 
-		it('Should have a "count" property set to 0', () => {
+		it('Should have a "count" property set to 0.', () => {
 			const resultId = Object.keys(actualResult)[0];
 
 			expect(actualResult[resultId].count).toEqual(0);
 		});
 
-		// it('should create a new counter object with a correctly incremented ID', () => {
-		// 	const stateBefore = {
-		// 			0 : {
-		// 				id: 0,
-		// 				count: 0
-		// 			}
-		// 		};
-		// 	const expectedResult = {
-		// 			0 : {
-		// 				id: 0,
-		// 				count: 0
-		// 			},
-		// 			1 : {
-		// 				id: 1,
-		// 				count: 0
-		// 			}
-		// 		};
-		// 	const actualResult = multiCounter(stateBefore, { type: TYPE.CREATE_COUNTER });
+		it('Should add a new counter object along side existing counter objects in the state.', () => {
+			const stateBeforeId = v4();
+			const stateBefore = {
+						[stateBeforeId] : {
+							id: stateBeforeId,
+							count: 0
+						}
+					};
 
-		// 	expect(actualResult).toMatchObject(expectedResult);
-		// });
+			const actualResult = multiCounter(stateBefore, { type: TYPE.CREATE_COUNTER });
+			const resultKeys = Object.keys(actualResult);
+
+			expect(resultKeys.length).toEqual(2);
+
+		});
 	});
 
-	// describe('Multi counter increment', () => {
-	// 	it('should increment the counter relative to the supplied ID', () => {
-	// 		const stateBefore = {
-	// 			0 : {
-	// 				id: 0,
-	// 				count: 0
-	// 			},
-	// 			1 : {
-	// 				id: 1,
-	// 				count: 0
-	// 			}
-	// 		};
-	// 		const action = {
-	// 			id: 1,
-	// 			type: TYPE.INCREMENT_MULTI_COUNTER
-	// 		};
-	// 		const expectedResult = {
-	// 			0 : {
-	// 				id: 0,
-	// 				count: 0
-	// 			},
-	// 			1 : {
-	// 				id: 1,
-	// 				count: 1
-	// 			}
-	// 		};
+	describe('Multi counter increment', () => {
+		it('Should increment the counter relative to the supplied ID', () => {
+			const id00 = v4();
+			const id01 = v4();
 
-	// 		expect(multiCounter(stateBefore, action)).toEqual(expectedResult);
-	// 	});
-	// });
+			const stateBefore = {
+				[id00] : {
+					id: id00,
+					count: 0
+				},
+				[id01] : {
+					id: id01,
+					count: 0
+				}
+			};
+			const action = {
+				id: id01,
+				type: TYPE.INCREMENT_MULTI_COUNTER
+			};
+			const expectedResult = {
+				...stateBefore,
+				[action.id]: {
+					...stateBefore[action.id],
+					count: 1
+				}
+			};
 
-	// describe('Multi counter decrement', () => {
-	// 	it('should decrement the counter relative to the supplied ID', () => {
-	// 		const stateBefore = {
-	// 			0 : {
-	// 				id: 0,
-	// 				count: 0
-	// 			},
-	// 			1 : {
-	// 				id: 1,
-	// 				count: 2
-	// 			}
-	// 		};
-	// 		const action = {
-	// 			id: 1,
-	// 			type: TYPE.DECREMENT_MULTI_COUNTER
-	// 		};
-	// 		const expectedResult = {
-	// 			0 : {
-	// 				id: 0,
-	// 				count: 0
-	// 			},
-	// 			1 : {
-	// 				id: 1,
-	// 				count: 1
-	// 			}
-	// 		};
+			expect(multiCounter(stateBefore, action)).toEqual(expectedResult);
+		});
+	});
 
-	// 		expect(multiCounter(stateBefore, action)).toEqual(expectedResult);
-	// 	});
-	// });
+	describe('Multi counter decrement', () => {
+		it('Should decrement the counter relative to the supplied ID', () => {
+			const id00 = v4();
+			const id01 = v4();
+
+			const stateBefore = {
+				[id00]: {
+					id: id00,
+					count: 0
+				},
+				[id01]: {
+					id: id01,
+					count: 3
+				}
+			};
+			const action = {
+				id: id01,
+				type: TYPE.DECREMENT_MULTI_COUNTER
+			};
+			const expectedResult = {
+				...stateBefore,
+				[action.id]: {
+					...stateBefore[action.id],
+					count: 2
+				}
+			};
+
+			expect(multiCounter(stateBefore, action)).toEqual(expectedResult);
+		});
+	});
 
 });
